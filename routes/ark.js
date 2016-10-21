080b7b37f2b630e5a3aff9801101d10cf6c0f79a
Stéphane Gully <stephane.gully@gmail.com>
2016-10-21 18:08:55 +0200

    adding a ARK/istexID mapping dump and connecting it to the routes

--- routes/ark.js
+++ routes/ark.js
@@ -4,10 +4,23 @@
 
 var path = require('path')
   , basename = path.basename(__filename, '.js')
-  , debug = require('debug')('istex-ark:' + basename);
+  , debug = require('debug')('istex-ark:' + basename)
+  , arkSplitter = require('../lib/ark-splitter.js')
+  , mapping = require('../mapping-dump.json');
 
 module.exports = function (req, res, next) {
-  var ark = req.originalUrl.slice(1);
+  var ark = arkSplitter(req.originalUrl.slice(1));
+
   debug('Requesting istexId mapped to this ARK: ' + ark);
-  return res.status(200).send(ark);
+  var istexId = mapping[ark.value];
+  if (istexId) {
+    debug('istexId found: ' + ark.value + ' -> ' + istexId);
+    return res.status(200).send({
+      "ark": ark,
+      "istexId": istexId
+    });
+  } else {
+    debug('istexId not found: ' + ark.value);
+    return res.status(404).send({ '_error': ark.value + ' do not have any corresponding istexId' });
+  }
 }
--- routes/istexid.js
+++ routes/istexid.js
@@ -4,10 +4,23 @@
 
 var path = require('path')
   , basename = path.basename(__filename, '.js')
-  , debug = require('debug')('istex-ark:' + basename);
+  , debug = require('debug')('istex-ark:' + basename)
+  , arkSplitter = require('../lib/ark-splitter.js')
+  , mapping = require('../mapping-dump.json');
 
 module.exports = function (req, res, next) {
   var istexId = req.originalUrl.slice(1);
+
   debug('Requesting ARK mapped to this istexId: ' + istexId);
-  return res.status(200).send(istexId);
+  var ark = arkSplitter(mapping[istexId]);
+  if (ark) {
+    debug('ARK found: ' + istexId + ' -> ' + ark.value);
+    return res.status(200).send({
+      "ark": ark,
+      "istexId": istexId
+    });
+  } else {
+    debug('ARK not found: ' + istexId);
+    return res.status(404).send({ '_error': istexId + ' do not have any corresponding ARK' });
+  }
 }
