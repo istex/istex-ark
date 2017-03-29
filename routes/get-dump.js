d370a9e8a2eafbbe4f7e96f6ec21831b0806f8cf
Stéphane Gully <stephane.gully@gmail.com>
2017-03-29 20:40:17 +0200

    add the total number of generated ARKs in the index.html page

--- routes/get-dump.js
+++ routes/get-dump.js
@@ -13,23 +13,38 @@ var path = require('path')
 
 module.exports.routing = function (app) {
 
-  // ex: GET http://ark.istex.fr/
   app.get('/dump',function (req, res) {
 
-    // loop over all the ark/istexid
-    redisClient.keys('ark*', function (err, arks) {
-      async.map(arks, function (ark, cb) {
-        redisClient.get(ark, function (err, istexId) {
-          cb(null, {
-            istexId: istexId,
-            ark:     ark
-          });
-        })
-      }, function (err, results) {
+    // count how much ARK in the Redis database
+    // divided by 2 because there is 2 kind of key
+    // for the same ARK: idistex & ark
+    redisClient.dbsize(function (err, count) {
+      var nbARKs = count / 2;
+
+      // loop over all the ark/istexid
+      // limit to the 100 first ARKs
+      // to get all the arks: redisClient.keys('ark*', function (err, arks) {
+      redisClient.send_command('SCAN', [0, 'COUNT', 100, 'MATCH', 'ark*'], function (err, arks) {
         if (err) return res.status(500).send(err);
-        res.status(200).send(results);
+        async.map(arks[1], function (ark, cb) {
+          redisClient.get(ark, function (err, istexId) {
+            cb(null, {
+              istexId: istexId,
+              ark:     ark
+            });
+          })
+        }, function (err, results) {
+          if (err) return res.status(500).send(err);
+          res.status(200).send({
+            totalCount: nbARKs,
+            items: results
+          });
+        });
       });
+
     });
+
+
 
   });
 
