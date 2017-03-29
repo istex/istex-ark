007e4a3ff92e6b873930eee4826290440ae1b1f1
François Parmentier <francois.parmentier@gmail.com>
2017-03-29 13:36:23 +0200

    Begin tests

---
+++ test/run.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const
+    pkg = require('../package.json')
+  , request = require('superagent')
+  , chai = require('chai')
+  , expect = chai.expect
+  ;
+
+describe(pkg.name + '/index.js test service', function() {
+
+  describe('post-istexid.js', function () {
+
+    it('checks the body length', function(done) {
+      request
+      .post('http://127.0.0.1:3000/')
+      .end(function(err, res) {
+        expect(err.status).to.be.not.null;
+        expect(err.status).to.equal(400);
+        done();
+      })
+    });
+
+    it('checks presence of corpusName', function(done) {
+      request
+      .post('http://127.0.0.1:3000/')
+      .send([{ idIstex: '1234567890123456789012345678901234567890', corpus: 'elsevier' }])
+      .end(function(err, res) {
+        expect(err).to.be.not.null;
+        expect(err.status).to.equal(400);
+        done();
+      });
+    })
+
+    it('returns an ARK for one idIstex', function(done) {
+      request
+      .post('http://127.0.0.1:3000/')
+      .send([{ idIstex: '1234567890123456789012345678901234567890', corpusName: 'elsevier' }])
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res.body['1234567890123456789012345678901234567890']).to.match(/^ark:\/67375\/6H6-[A-Z0-9]{8}-[A-Z0-9]/);
+        done();
+      });
+    })
+
+    it('returns an ARK for two idIstex', function(done) {
+      request
+      .post('http://127.0.0.1:3000/')
+      .send([{ idIstex: '1234567890123456789012345678901234567890', corpusName: 'elsevier' },
+             { idIstex: '1234567890123456789012345678901234567891', corpusName: 'wiley' }
+            ])
+      .end(function(err, res) {
+        expect(err).to.be.null;
+        expect(res.body['1234567890123456789012345678901234567890']).to.be.not.null;
+        expect(res.body['1234567890123456789012345678901234567890'].length).to.equal(25);
+        done();
+      });
+    })
+
+  });
+
+});
